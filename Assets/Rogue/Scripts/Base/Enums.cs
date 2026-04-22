4806287792102224bd1fddbaa8e5545c388e497f
Renat <painren@mail.ru>
2026-04-22 10:45:27 +0300

    final fix

--- Assets/Rogue/Scripts/Base/Enums.cs
+++ Assets/Rogue/Scripts/Base/Enums.cs
@@ -64,6 +64,7 @@ public static class Enums
     public enum PanelType
     {
         Inventory,
-        Character
+        Character,
+        Factions
     }
 }
---
+++ Assets/Rogue/Scripts/UI/Screens/TestScreen/Panels/FactionsPanelVIew.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+public class FactionsPanelVIew : PanelUI
+{
+    public override void Deinitialize()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Initialize(List<CharacterViewModel> characterViewModels)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
